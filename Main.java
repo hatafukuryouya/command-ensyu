651b90e94dca79c2f226c700c39e9a237bab537c
ryouya <r.hatafuku.sys22@morijiyobi.ac.jp>
2022-11-10 10:46:07 +0900

    BMI計算の変更を追記

--- Main.java
+++ Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       Scanner stdIn = new Scanner(System.in);
-   
-      System.out.print("身長(m):");
-      double height = stdIn.nextDouble(); 
-      System.out.print("体重:");
-      double weight = stdIn.nextDouble(); 
-   
-      double bmi = weight / Math.pow(height, 2);
-   
-      System.out.println("BMI値は" + Math.floor(bmi*100)/100 +"です。" );
+        for(int i=1;i<=2;i++){
+        System.out.print(i+"人目の身長(m):");
+        double height = stdIn.nextDouble(); 
+        System.out.print(i+"人目の体重:");
+        double weight = stdIn.nextDouble(); 
+ 
+        double bmi = weight / Math.pow(height, 2);
+ 
+        System.out.println(i+"人目のBMI値は" + Math.floor(bmi*100)/100 + "です。");
+        }
     }
-  }
+}
